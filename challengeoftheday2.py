e82c823ce9b127ab74e4c87d195aeaf38857972b
Jortuk <Jordan.Taylor@academytrainee.com>
2020-05-05 17:20:16 +0100

    Completed challenge of the day

---
+++ challengeoftheday2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+factorial = 1
+#User input
+
+num = int(input("Please enter a number: "))
+
+if num < 0:
+    print("Sorry, factorials do not exist for negative numbers.")
+elif num == 0:
+    print("The factorial of 0 is 1") 
+else:
+    for i in range(1,num+1):
+        factorial = factorial*i
+    print("The factorial of ",num," is ",factorial)
