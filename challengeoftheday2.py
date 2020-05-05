e4c57430b6da267e11eead291ec304d75758b02d
Jortuk <Jordan.Taylor@academytrainee.com>
2020-05-05 17:28:29 +0100

    Completed challenge of the day

--- challengeoftheday2.py
+++ challengeoftheday2.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 
 factorial = 1
-#User input
 
-num = int(input("Please enter a number: "))
+#While session is True, input will be asked for again after the answer until file is closed (Crtl + C)
+session = True
+while session:
 
-if num < 0:
-    print("Sorry, factorials do not exist for negative numbers.")
-elif num == 0:
-    print("The factorial of 0 is 1") 
-else:
-    for i in range(1,num+1):
-        factorial = factorial*i
-    print("The factorial of ",num," is ",factorial)
+    #User input
+    num = int(input("Please enter a number: "))
+
+    #Determining factorial
+    if num < 0:
+        print("Sorry, factorials do not exist for negative numbers.")
+    elif num == 0:
+        print("The factorial of 0 is 1") 
+    else:
+        for i in range(1,num+1):
+            factorial = factorial*i
+        print("The factorial of ",num," is ",factorial)
+       
